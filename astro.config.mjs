d5220380090bbd73bc8692431146e3ce1e42b8fc
Hugo Nogueira <hugo.nogueira@lego.com>
2024-06-09 17:19:36 +0100

    bye cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
-
 import preact from "@astrojs/preact";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), preact()],
-  adapter: cloudflare({
-    runtime: {
-      mode: "local",
-      type: "pages",
-      bindings: {
-        COMICS: {
-          type: "kv"
-        }
-      }
-    }
-  })
+  adapter: node({
+    mode: "standalone",
+  }),
 });
