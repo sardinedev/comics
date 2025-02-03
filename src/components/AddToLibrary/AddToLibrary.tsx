3f92e079ff93faec111b27df4a4a29c4c768f880
Hugo Nogueira <hnogueira@marabyte.com>
2025-02-03 00:07:18 +0000

    tailwind changes

--- src/components/AddToLibrary/AddToLibrary.tsx
+++ src/components/AddToLibrary/AddToLibrary.tsx
@@ -16,7 +16,7 @@ export function AddToLibrary({ seriesId }: { seriesId: string }) {
     <button
       onClick={onClick}
       type="button"
-      class="rounded-full bg-indigo-600 px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      class="rounded-full bg-indigo-600 px-3.5 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     >
       {buttonText}
     </button>
