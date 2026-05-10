3940d3060b0846790ac9f8f1c0cfd553cb35ab17
Hugo Nogueira <hnogueira@marabyte.com>
2026-05-10 19:45:15 +0100

    refactor(env): streamline environment variable loading with dedicated loaders

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,26 +1,27 @@
 /// <reference types="astro/client" />
 
-interface ImportMetaEnv {
-	readonly COMICVINE_API_KEY: string;
-	readonly PUBLIC_COMICVINE_URL: string;
-	readonly ELASTIC_INDEX: string;
-	readonly ELASTIC_URL: string;
-	readonly ELASTIC_API_KEY: string;
-	readonly MYLAR_URL: string;
-	readonly MYLAR_API_KEY: string;
-	readonly COVERS_DIR: string;
-	readonly PUBLIC_URL: string;
-	readonly ATPROTO_PRIVATE_KEY_JWK: string;
-	readonly AUTH_ALLOWED_DID: string;
-	readonly AUTH_SESSION_SECRET: string;
-}
+export {};
 
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
+declare global {
+	interface ImportMetaEnv {
+		readonly COMICVINE_API_KEY: string;
+		readonly PUBLIC_COMICVINE_URL: string;
+		readonly ELASTIC_INDEX: string;
+		readonly ELASTIC_URL: string;
+		readonly ELASTIC_API_KEY: string;
+		readonly MYLAR_URL: string;
+		readonly MYLAR_API_KEY: string;
+		readonly COVERS_DIR: string;
+		readonly PUBLIC_URL: string;
+		readonly ATPROTO_PRIVATE_KEY_JWK: string;
+		readonly AUTH_ALLOWED_DID: string;
+		readonly AUTH_SESSION_SECRET: string;
+		readonly DEV_BYPASS_AUTH: string;
+	}
 
-declare namespace App {
-	interface Locals {
-		did: string;
+	namespace App {
+		interface Locals {
+			did: string;
+		}
 	}
 }
