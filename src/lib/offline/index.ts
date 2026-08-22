e2315c267a1194bd2605ff675b44e763f1ee0e4f
Hugo Nogueira <hnogueira@marabyte.com>
2026-08-22 12:32:22 +0100

    feat: sync reading progress by timestamp

--- src/lib/offline/index.ts
+++ src/lib/offline/index.ts
@@ -2,5 +2,6 @@ export * from "./cache-names";
 export * from "./clear";
 export * from "./database";
 export * from "./outbox";
+export * from "./progress-sync";
 export * from "./search";
 export * from "./types";
